7db62c50d7f3142eab3fc1633cca35b8cb7de795
pervezmalik <46785383+pervezmalik@users.noreply.github.com>
2019-09-27 15:08:41 +0530

    Added server connection

--- info.php
+++ info.php
@@ -1,5 +1,14 @@
-<?php 
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
 
-phpinfo();
+// Create connection
+$conn = new mysqli($servername, $username, $password);
 
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>
